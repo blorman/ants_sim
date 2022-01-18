c4c2d449a20cf0522b67d64834e4f11681a5ea0a
Tim Hollingsworth <timhollingsworth@gmail.com>
2022-01-18 14:51:13 +0100

    moved plugin to new module

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,10 @@
+mod ants_plugin;
+
 use bevy::prelude::*;
-
-#[derive(Component)]
-struct Ant;
-
-#[derive(Component)]
-struct Name(String);
-
-struct GreetTimer(Timer);
-
-fn add_ants(mut commands: Commands) {
-    commands.spawn().insert(Ant).insert(Name("Bob".to_string()));
-    commands.spawn().insert(Ant).insert(Name("Pam".to_string()));
-    commands.spawn().insert(Ant).insert(Name("Max".to_string()));
-}
-
-fn greet_ants(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Ant>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_ants)
-            .add_system(greet_ants);
-    }
-}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_plugin(ants_plugin::AntsPlugin)
         .run()
 }
