e7febd33b57268a37f6b6e90c7955ec2b15aa1e3
Tim Hollingsworth <timhollingsworth@gmail.com>
2022-01-24 10:25:43 +0100

    tweaked build flags to improve performance

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod ants_plugin;
 mod console_debug_plugin;
 
+// use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        // .add_plugin(LogDiagnosticsPlugin::default())
+        // .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(console_debug_plugin::ConsoleDebugPlugin)
         .add_plugin(ants_plugin::AntsPlugin)
         .run()
